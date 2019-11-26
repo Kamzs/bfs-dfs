465fe49f5dd0162624de05df7bd1a1924bf33904
Kamil Ambroziak <k.ambroziak@samsung.local>
2019-11-26 16:09:24 +0100

    save

--- src/com/ambro/Airplanes.java
+++ src/com/ambro/Airplanes.java
@@ -6,25 +6,31 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Airplanes {
-
     int result=-1;
 
     public static void main(String[] args) throws FileNotFoundException {
 
         long a = System.currentTimeMillis();
+        Airplanes airplanes = new Airplanes();
 
-        int[][] allSteps_events = LOAD_DATA();
-        int init_step =0;
-        int index_of_plane =2;
-        int current_result =0;
+        Object[] listOfCases = airplanes.LOAD_DATA();
+        int init_step = 0;
+        int index_of_plane = 2;
+        int current_result = 0;
         boolean detonated = false;
 
-        System.out.println(Arrays.deepToString(allSteps_events));
-        Airplanes airplanes = new Airplanes();
-        airplanes.proceed(airplanes,allSteps_events,init_step, index_of_plane,current_result,detonated);
-        System.out.println(airplanes.result);
+        for (int f=0; f < listOfCases.length;f++) {
+            airplanes.result = -1;
+
+            int[][] allSteps_events = (int[][])listOfCases[f];
+
+            airplanes.proceed(airplanes, allSteps_events, init_step, index_of_plane, current_result, detonated);
+            System.out.println(airplanes.result);
+
+        }
+
         long b = System.currentTimeMillis();
-        System.out.println("runtime was : " + (b - a) +" ms");
+        System.out.println("runtime was : " + (b - a) + " ms");
 
     }
 
@@ -111,30 +117,39 @@ public class Airplanes {
    //     else System.out.println("ship destroyed");
     }
 
-    public static int[][] LOAD_DATA() throws FileNotFoundException {
+    public Object[] LOAD_DATA() throws FileNotFoundException {
 
 
         System.setIn(new FileInputStream("src/com/ambro/airplanes"));
         Scanner input = new Scanner(System.in);
-        int no_of_rows = input.nextInt();
 
-        int[][] allSteps_events = new int[no_of_rows][5];
+        int no_of_cases = input.nextInt();
+     //   ListFiFO listOfCases = new ListFiFO();
+        Object[] objects = new Object[no_of_cases];
 
-        for (int k = no_of_rows-1; k >= 0; k--) {
-            for (int l = 0; l < 5; l++) {
-                allSteps_events[k][l] = input.nextInt();
+        for (int no =0; no < no_of_cases; no++){
+            int no_of_rows = input.nextInt();
+            int[][] allSteps_events = new int[no_of_rows][5];
+
+            for (int k = no_of_rows-1; k >= 0; k--) {
+                for (int l = 0; l < 5; l++) {
+                    allSteps_events[k][l] = input.nextInt();
+                }
             }
+            objects[no] = allSteps_events;
         }
-        return allSteps_events;
+
+
+        return objects;
     }
 
     class ListFiFO{
         int SIZE = 1;
-        int [] list = new int[SIZE];
+        Object [] list = new Object[SIZE];
         int index_of_first_el = 0;
         int current_index_for_new = 0;
 
-        void add_back(int x){
+        void add_back(Object x){
             if (current_index_for_new<SIZE){
             list[current_index_for_new]=x;
             current_index_for_new++;
@@ -146,19 +161,19 @@ public class Airplanes {
             }
         }
 
-        int pop_first_el (){
+        Object pop_first_el (){
             index_of_first_el++;
             return list[index_of_first_el-1];
         }
 
 
-        int get_by_index (int index){
+        Object get_by_index (int index){
             return list[index];
         }
 
         void make_bigger(){
             SIZE *=2;
-            int [] newTable = new int [SIZE];
+            Object [] newTable = new Object [SIZE];
             for (int i=0; i < list.length; i++){
                 newTable[i] = list[i];
             }
